262a621d9cda7f41fca2458651155c22fe327ec2
Michel Rijvers <mjlprijv@avans.nl>
2015-11-03 13:18:33 +0100

    class Treatment and Session

--- Java Physio Shared Files/src/edu/avans/ivh5/shared/model/domain/Session.java
+++ Java Physio Shared Files/src/edu/avans/ivh5/shared/model/domain/Session.java
@@ -5,10 +5,66 @@
  */
 package edu.avans.ivh5.shared.model.domain;
 
+import java.util.Date;
+
 /**
  *
  * @author bernd_000
  */
 public class Session {
+    private Date Date;
+    private Date StartTime;
+    private Date EndTime;
+    private String Notes;
+    private String TreatmentID;
     
+    public Session(Date Date, Date StartTime, Date EndTime, String Notes, String TreatmentID)
+    {
+        this.Date = Date;
+        this.StartTime = StartTime;
+        this.EndTime = EndTime;
+        this.Notes = Notes;
+        this. TreatmentID = TreatmentID;
+    }
+
+    public Date getDate() {
+        return Date;
+    }
+
+    public void setDate(Date Date) {
+        this.Date = Date;
+    }
+
+    public Date getStartTime() {
+        return StartTime;
+    }
+
+    public void setStartTime(Date StartTime) {
+        this.StartTime = StartTime;
+    }
+
+    public Date getEndTime() {
+        return EndTime;
+    }
+
+    public void setEndTime(Date EndTime) {
+        this.EndTime = EndTime;
+    }
+
+    public String getNotes() {
+        return Notes;
+    }
+
+    public void setNotes(String Notes) {
+        this.Notes = Notes;
+    }
+
+    public String getTreatmentID() {
+        return TreatmentID;
+    }
+
+    public void setTreatmentID(String TreatmentID) {
+        this.TreatmentID = TreatmentID;
+    }
+     
 }
